400221f614a53161d0519a3e90de39e4e7bec4f8
TMalevolent <35205383+TMalevolent@users.noreply.github.com>
2020-09-16 14:32:23 -0700

    Update drawer_code.ino

--- drawer_code.ino
+++ drawer_code.ino
@@ -88,7 +88,7 @@ void reset_friction() {
 }
 
 void read_handle_val() {
-  convert_force(analogRead(fsr_1)); Serial.print(" ");
+  /*convert_force(analogRead(fsr_1)); Serial.print(" ");
   convert_force(analogRead(fsr_2)); Serial.print(" ");
   convert_force(analogRead(fsr_3)); Serial.print(" ");
   convert_force(analogRead(fsr_4)); Serial.print(" ");
@@ -99,7 +99,19 @@ void read_handle_val() {
   convert_force(analogRead(fsr_9)); Serial.print(" ");
   convert_force(analogRead(fsr_10)); Serial.print(" ");
   convert_force(analogRead(fsr_11)); Serial.print(" ");
-  convert_force(analogRead(fsr_12)); Serial.print(" ");
+  convert_force(analogRead(fsr_12)); Serial.print(" ");*/
+  Serial.println(analogRead(fsr_1));
+  Serial.println(analogRead(fsr_2));
+  Serial.println(analogRead(fsr_3));
+  Serial.println(analogRead(fsr_4));
+  Serial.println(analogRead(fsr_5));
+  Serial.println(analogRead(fsr_6));
+  Serial.println(analogRead(fsr_7));
+  Serial.println(analogRead(fsr_8));
+  Serial.println(analogRead(fsr_9));
+  Serial.println(analogRead(fsr_10));
+  Serial.println(analogRead(fsr_11));
+  Serial.println(analogRead(fsr_12));
 }
 
 void get_force_val() {
@@ -114,28 +126,28 @@ void get_force_val() {
   }
 }
 
-void convert_force(int reading){
-  int voltage;
-  unsigned long resistance;
-  unsigned long conductance;
-  long force;
-  int resistor = 10000; //10k resistor
-  int Vcc = 3300; //in mV, using 3.3v in voltage divider circuit
-  
-  voltage = map(reading, 0, 616, 0, 3300); //re-maps voltage value. 616 is fsr max output when using 10k resistor and 3.3v
+/*void convert_force(int reading){
+  float voltage;
+  float resistance;
+  float conductance;
+  float force;
+  int resistor = 10000; //we use a 10k resistor in the voltage divider circuit
+  int Vcc = 5000; //in mV, we used 5v
+  voltage = map(reading, 0, 1023, 0, Vcc); //re-maps voltage val to fsr reading range. 1023 is FSR max output when using 10k resistor and 5v
+  //voltage = long(reading * Vcc) / 616; 
   if(voltage == 0){
-    Serial.print(0);
+    Serial.print(0); //no force
   }
   else{
     //fsr = ((Vcc - v) * R) / v
-    resistance = Vcc - voltage;
-    reistance *= resistor;
+    resistance = Vcc -  voltage;
+    resistance *= resistor;
     resistance /= voltage;
-    
-    conductance = 1000000; //micro-ohms
+
+    conductance = 100000; //in micro-Ohms
     conductance /= resistance;
-    //the value of the FSR's is a parabola based on the force applied
-    //this splits the parabola into two linear equations for approximation
+    //the value of the FSR's is a parabola based on the force applied.
+    //this splits the parabola into two linear equations for linear approximation
     if(conductance <= 1000){
       force = conductance / 80;
     }
@@ -145,8 +157,7 @@ void convert_force(int reading){
     }
     Serial.print(force);
   }
-}
-
+}*/
 /*---------------Main Code-------------*/
 
 void setup() {
@@ -192,18 +203,16 @@ void loop() {
   //for testing purposes only:
   time = millis();
   time_stop = time + 15000;
-  while (time < time_stop) { // main loop for getting fsr values and pos of drawer
+  while ( true /*time < time_stop*/) { // main loop for getting fsr values and pos of drawer
     tof.rangingTest(&measure, false);
     if (measure.RangeMilliMeter <= start_pos) {
-      Serial.print("Distance Drawer is out: 0mm");
+      Serial.println(0);
     }
     else {
-      Serial.print("Distance Drawer is out: "); Serial.print((measure.RangeMilliMeter - start_pos)); Serial.print("mm");
+     Serial.println((measure.RangeMilliMeter - start_pos));
     }
-    Serial.print(". Time: "); Serial.print(time);
     read_handle_val();
-    Serial.println();
-    delay(50);
+    Serial.println(time);
     time = millis();
   }
   reset_friction();
