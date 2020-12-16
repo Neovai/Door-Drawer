76e32e44b91333f982b236061f2a12efc6598877
TMalevolent <roberyan@oregonstate.edu>
2020-12-16 14:26:40 -0800

    modified:   Arduino_data_collection.py 	modified:   Drawer_data_collection/Drawer_data_collection.ino

--- Arduino_data_collection.py
+++ Arduino_data_collection.py
@@ -4,16 +4,23 @@ import csv
 
 ser = serial.Serial('COM10') #change com port for arduino
 ser.flushInput()
+data = []
 
 while True:
     try:
         #print("running...")
         ser_bytes = ser.readline()
         decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+        data.append([decoded_bytes])
         print(decoded_bytes) #for testing purposes
-        with open("test_data.csv","a") as f: #"a" stands for appending data to same csv file
-            writer = csv.writer(f,delimiter=",")
-            writer.writerow([decoded_bytes])
+        #with open("test_data.csv","a") as f: #"a" stands for appending data to same csv file
+         #   writer = csv.writer(f,delimiter=",")
+          #  writer.writerow([decoded_bytes])
     except:
         print("keyboard Interrupt") #press ctrl-c to break the loop
         break
+    for i in len(data):
+        with open("test_data.csv","a") as f: #"a" stands for appending data to same csv file
+            writer = csv.writer(f,delimiter=",")
+            writer.writerow([decoded_bytes])
+
