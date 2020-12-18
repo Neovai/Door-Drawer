01a169eb1449dd5ac212e0384b9a3422c87409b8
TMalevolent <roberyan@oregonstate.edu>
2020-12-17 16:31:12 -0800

    modified:   Arduino_data_collection.py

--- Arduino_data_collection.py
+++ Arduino_data_collection.py
@@ -12,10 +12,6 @@ while timer > time.perf_counter():
     ser_bytes = ser.readline()
     decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
     data.append([decoded_bytes])
-    print(decoded_bytes)
-   # with open("data.csv","a") as f: #"a" stands for appending data to same csv file
-    #    writer = csv.writer(f,delimiter=",")
-     #   writer.writerow([decoded_bytes])
 for i in range(0,len(data)):
     with open("test_data.csv","a") as f: 
         writer = csv.writer(f,delimiter=",")
